565b9c87fde7580fb0f5cfca678b44915f912546
shubham <shubham22121@gmail.com>
2022-10-15 03:56:14 +0530

    [style] formated

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,33 +1,28 @@
-const {
-    login,
-    register,
-    getMe
-} = require('../services/auth.service');
+const { login, register, getMe } = require("../services/auth.service");
 
 export async function registerUserHandler(req, res) {
-    try {
-        const user = await register(req.body);
-        res.json(user);
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
+  try {
+    const user = await register(req.body);
+    res.json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 }
 
 export async function loginUserHandler(req, res) {
-    try {
-        const user = await login(req.body);
-        res.json(user);
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
+  try {
+    const user = await login(req.body);
+    res.json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 }
 
-
 export async function getMeHandler(req, res) {
-    try {
-        const user = await getMe(req.user.id);
-        res.json(user);
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
+  try {
+    const user = await getMe(req.user.id);
+    res.json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 }
--- src/middleware/validate.js
+++ src/middleware/validate.js
@@ -1,29 +1,23 @@
-const {
-    check
-} = require('express-validator');
-
+const { check } = require("express-validator");
 
 const validate = (method) => {
-    switch (method) {
-        case 'createUser': {
-            return [
-                check('name', 'Name is required').exists(),
-                check('email', 'Email is required').exists(),
-                check('email', 'Email is invalid').isEmail(),
-                check('password', 'Password is required').exists()
-            ]
-        }
-        case 'loginUser': {
-            return [
-                check('email', 'Email is required').exists(),
-                check('email', 'Email is invalid').isEmail(),
-                check('password', 'Password is required').exists()
-            ]
-        }
-        
-
+  switch (method) {
+    case "createUser": {
+      return [
+        check("name", "Name is required").exists(),
+        check("email", "Email is required").exists(),
+        check("email", "Email is invalid").isEmail(),
+        check("password", "Password is required").exists(),
+      ];
     }
-
-}
+    case "loginUser": {
+      return [
+        check("email", "Email is required").exists(),
+        check("email", "Email is invalid").isEmail(),
+        check("password", "Password is required").exists(),
+      ];
+    }
+  }
+};
 
 module.exports = validate;
